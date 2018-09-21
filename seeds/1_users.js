08776c99402742091f682c540f81a1774d158fa3
Jane Jeon <JaneJeon9719@gmail.com>
2018-09-21 16:49:34 -0400

    updated seed

--- seeds/1_users.js
+++ seeds/1_users.js
@@ -1,6 +1,5 @@
 const User = require("../src/models/user"),
   user = {
-    id: 1,
     email: "test@example.com",
     password: "123456789"
   }
@@ -12,7 +11,7 @@ exports.seed = async knex => {
   const instance = User.fromJson(user)
   await instance.$beforeInsert()
   await knex("users").insert([
-    instance,
+    Object.assign(instance, { id: 1 }),
     // this person is strictly used to test rights for notebooks table
     {
       id: 2,
